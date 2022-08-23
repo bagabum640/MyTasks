22e612332b2c690c2c0947f74a28e05227027d03
Александр Пыстин <bagabum640gmail.com>
2022-08-23 14:15:41 +0300

    test push

--- Task/BraveNewWorld/Program.cs
+++ Task/BraveNewWorld/Program.cs
@@ -6,7 +6,7 @@ namespace BraveNewWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
