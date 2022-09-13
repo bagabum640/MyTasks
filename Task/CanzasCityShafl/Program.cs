36b1430a44c94c07fc06d8f67b5129c8d79d1eec
Александр Пыстин <bagabum640gmail.com>
2022-09-13 12:26:28 +0300

    Added task Shaffle

---
+++ Task/CanzasCityShafl/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanzasCityShafl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            WriteArray(arr);
+            Shuffle(arr);
+            WriteArray(arr);
+        }
+
+        static void Shuffle(int [] enteredArray)
+        {
+            Random random = new Random();
+            int buffer;
+            int index;
+
+            for (int i = 0; i < enteredArray.Length; i++)
+            {
+                index = random.Next(i, enteredArray.Length);                
+                buffer = enteredArray[i];
+                enteredArray[i] = enteredArray[index];
+                enteredArray[index] = buffer;
+            }
+        }
+
+        static void WriteArray(int [] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
