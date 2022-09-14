4243aebb3673c69a128c194a2f515529a6d79c75
Александр Пыстин <bagabum640gmail.com>
2022-09-14 05:02:14 +0300

    Added ShopQueue

---
+++ Task/ShopQueue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cashBalance = 0;
+            Queue<int> purchasesPrice = new Queue<int>();
+
+            BuyAnything(purchasesPrice);
+
+            while (purchasesPrice.Count > 0)
+            {
+                SellAnything(ref cashBalance, purchasesPrice);
+            }
+
+            Console.WriteLine($"Денег в кассе: {cashBalance}\n");
+        }
+        static void BuyAnything(Queue<int> purchasesPrice)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 7; i++)
+            {
+                purchasesPrice.Enqueue(random.Next(20, 700));
+            }            
+        }
+        static void SellAnything(ref int cashBalance, Queue<int> purchasesPrice)
+        {
+            Console.Write($"Денег в кассе: {cashBalance}\n");
+            Console.WriteLine($"Сумма текущей покупки: {purchasesPrice.Peek()}");
+            cashBalance += purchasesPrice.Dequeue();
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+    }
+}
