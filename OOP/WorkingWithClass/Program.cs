628c2b657a54824ab3075797cd2388112a270c9a
Александр Пыстин <bagabum640gmail.com>
2022-10-01 12:53:35 +0300

    Add WorkingWithClass

---
+++ OOP/WorkingWithClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player newPlayer = new Player();
+            newPlayer.ShowStats();
+        }
+    }
+    class Player
+    {
+        public int Health;
+        public int Armor;
+        public int Damage;
+
+        public Player()
+        {
+            Health = 100;
+            Armor = 20;
+            Damage = 35;
+        }
+
+        public Player(int health, int armor, int damage)
+        {
+            Health = health;
+            Armor = armor;
+            Damage = damage;
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine($"Здоровье: {Health}\nБроня: {Armor}\nУрон: {Damage}");
+        }
+    }
+}
