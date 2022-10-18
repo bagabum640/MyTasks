a08fe8f384bcab22eab2a1a4cfd04fb2b0065630
Александр Пыстин <bagabum640gmail.com>
2022-10-18 15:22:41 +0300

    fixed BookStorage

--- OOP/BookStorage/Program.cs
+++ OOP/BookStorage/Program.cs
@@ -18,8 +18,8 @@ namespace BookStorage
 
             bool isExit = false;
             string command;
-            BookStorage bookStorage = new BookStorage();            
-            
+            BookStorage bookStorage = new BookStorage();
+
             Console.WriteLine($"Введите:\n{AddBookCommand} - чтобы добавить книгу\n{DeleteBookCommand} - чтобы удалить книгу\n" +
                 $"{ShowBooksCommand} - чтобы показать все книги\n{ShowBooksByParameterCommand} - показать все книги по указанному параметру\n{ExitCommand} - чтобы выйти\n");
 
@@ -60,7 +60,7 @@ namespace BookStorage
 
     class BookStorage
     {
-        private List<Book> _storage = new List<Book>();        
+        private List<Book> _storage = new List<Book>();
 
         public BookStorage()
         {
@@ -79,23 +79,17 @@ namespace BookStorage
 
             _storage.Add(new Book(name, writter, yearOfIssue));
         }
-        
-        public void Delete ()
-        {
-            Console.Write("Введите название книги: ");
-            string name = Console.ReadLine();
 
-            for (int i = 0; i < _storage.Count; i++)
+        public void Delete()
+        {
+            if (TrySearchByName(out Book book))
             {
-                if (name == _storage[i].Name)
-                {
-                    _storage.RemoveAt(i);
-                    Console.WriteLine($"Книга удалена.");
-                }
-                else
-                {
-                    Console.WriteLine("Такой книги нет.");
-                }
+                _storage.Remove(book);
+                Console.WriteLine($"Книга удалена.");
+            }
+            else
+            {
+                Console.WriteLine("Такой книги нет.");
             }
         }
 
@@ -109,29 +103,107 @@ namespace BookStorage
 
         public void ShowAt()
         {
-            Console.Write("Введите параметр искомой книги: ");
-            string parameter = Console.ReadLine();
+            const char NameParameter = 'n';
+            const char WritterParameter = 'w';
+            const char YearParameter = 'y';
+
             bool wasNotFound = true;
 
-            foreach (var book in _storage)
+            Console.Write($"Нажмите для поиска\n{NameParameter} - по названию книги\n{WritterParameter} - по автору\n{YearParameter} - по году выпуска\n\n");
+            char parameter = Console.ReadKey(true).KeyChar;
+
+            switch (parameter)
             {
-                if (book.SearchedBook(parameter))
-                {
-                    book.Show();
-                    wasNotFound = false;
-                }                
-            }
+                case NameParameter:
+                    SearchByName(ref wasNotFound);
+                    break;
+
+                case WritterParameter:
+                    SearchByWritter(ref wasNotFound);
+                    break;
+
+                case YearParameter:
+                    SearchByYear(ref wasNotFound);
+                    break;
+
+                default:
+                    Console.WriteLine("Неверный параметр!");
+                    return;                    
+            }            
 
             if (wasNotFound)
             {
                 Console.WriteLine("Книг по заданному параметру не найдено.");
             }
         }
+
+        public bool TrySearchByName(out Book book)
+        {
+            Console.Write("Введите название книги: ");
+            string name = Console.ReadLine();
+
+            foreach (var _book in _storage)
+            {
+                if (name == _book.Name)
+                {
+                    book = _book;
+                    return true;
+                }
+            }
+
+            book = null;
+            return false;
+        }
+
+        public void SearchByName(ref bool wasNotFound)
+        {
+            Console.Write("Введите название книги: ");
+            string name = Console.ReadLine();
+
+            foreach (var book in _storage)
+            {
+                if (book.Name == name)
+                {
+                    book.Show();
+                    wasNotFound = false;
+                }
+            }
+        }
+
+        public void SearchByWritter(ref bool wasNotFound)
+        {            
+            Console.Write("Введите автора: ");
+            string writter = Console.ReadLine();
+
+            foreach (var book in _storage)
+            {
+                if (book.Writter == writter)
+                {
+                    book.Show();
+                    wasNotFound = false;
+                }
+            }
+        }
+
+        public void SearchByYear(ref bool wasNotFound)
+        {
+            Console.Write("Введите год выпуска: ");
+            string year = Console.ReadLine();
+
+            foreach (var book in _storage)
+            {
+                if (book.YearOfIssue == year)
+                {
+                    book.Show();
+                    wasNotFound = false;
+                }
+            }
+        }
     }
 
     class Book
     {
-        public string Name  { get; private set; }
+        public string Name { get; private set; }
         public string Writter { get; private set; }
         public string YearOfIssue { get; private set; }
 
