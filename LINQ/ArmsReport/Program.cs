9328f35b37782b6f6bffec8f6c8f24274294a0b3
Александр Пыстин <bagabum640gmail.com>
2023-06-06 08:51:18 +0300

    Added 2 last tasks

---
+++ LINQ/ArmsReport/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArmsReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Army army = new Army();
+
+            army.Work();
+        }
+    }
+
+    class Army
+    {
+        private readonly List<Soldier> _soldiers = new List<Soldier>();
+
+        public Army()
+        {
+            _soldiers.Add(new Soldier("Борис", "Винтовка", "Сержант", 18));
+            _soldiers.Add(new Soldier("Иван", "Автомат", "Рядовой", 9));
+            _soldiers.Add(new Soldier("Николай", "Винтовка", "Сержант", 15));
+            _soldiers.Add(new Soldier("Тимофей", "Гранатамет", "Рядовой", 8));
+            _soldiers.Add(new Soldier("Артем", "Автомат", "Лейтенант", 23));
+            _soldiers.Add(new Soldier("Василий", "Огнемет", "Рядовой", 12));
+        }
+
+        public void Work()
+        {
+            var soldierData = _soldiers.Select(soldier => soldier.Name +"\t: "+ soldier.Rank);
+
+            foreach (var data in soldierData)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+
+    class Soldier
+    {
+        public Soldier(string name, string armament, string rank , int lifeTime)
+        {
+            Name = name;
+            Armament = armament;
+            Rank = rank;
+            LifeTime = lifeTime;
+        }
+
+        public string Name { get; private set; }
+        public string Armament { get; private set; }
+        public string Rank { get; private set; }
+        public int LifeTime { get; private set; }        
+    }
+}
---
+++ LINQ/UnificationTroops/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnificationTroops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Army army = new Army();
+
+            army.Work();
+        }
+    }
+
+    class Army
+    {
+        private readonly List<Soldier> _firstSquad = new List<Soldier>();
+        private readonly List<Soldier> _secondSquad = new List<Soldier>();
+
+        public Army()
+        {
+            _firstSquad.Add(new Soldier("Иванов Борис"));
+            _firstSquad.Add(new Soldier("Серов Иван"));
+            _firstSquad.Add(new Soldier("Петров Николай"));
+            _firstSquad.Add(new Soldier("Большаков Тимофей"));
+            _firstSquad.Add(new Soldier("Безруков Артем"));
+            _firstSquad.Add(new Soldier("Колбасин Василий"));
+            _secondSquad.Add(new Soldier("Фликов Семен"));
+            _secondSquad.Add(new Soldier("Горов Виталий"));
+            _secondSquad.Add(new Soldier("Шиков Алексей"));
+            _secondSquad.Add(new Soldier("Иванов Константин"));
+        }
+
+        public void Work()
+        {      
+            var resultSquad = _secondSquad.Union(_firstSquad.Where(soldier => soldier.Name.StartsWith("Б")));
+
+            foreach (var soldier in resultSquad)
+            {
+                Console.WriteLine(soldier.Name);
+            }
+        }
+    }
+
+    class Soldier
+    {
+        public Soldier(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+    }
+}
