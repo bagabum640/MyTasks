25e80e1f788a102fac0707fb4346feaad9e0dab9
nomisceyes <157041379+nomisceyes@users.noreply.github.com>
2024-07-10 11:43:01 +0300

    Fix CubeExplosion

--- UnityProjects/3Dproject/Assets/Scripts/Cube.cs
+++ UnityProjects/3Dproject/Assets/Scripts/Cube.cs
@@ -2,43 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CubeExplosion))]
+
 public class Cube : MonoBehaviour
 {
     [HideInInspector]
     [SerializeField] private float _cloneChance = 100;
-    [SerializeField] private float _explosionForce = 100;
-    [SerializeField] private float _explosionRadius = 100;
+    
+    private CubeExplosion _explosion;
 
-    private void OnMouseUpAsButton() => StartCoroutine(CreateCubes());
+    public Rigidbody Rigidbody { get; private set; }
+    public MeshRenderer MeshRenderer { get; private set; }
 
-    private IEnumerator CreateCubes()
+    private void OnEnable()
     {
-        if (_cloneChance > Random.Range(0, 100))
+        Rigidbody = GetComponent<Rigidbody>();
+        MeshRenderer = GetComponent<MeshRenderer>();
+        _explosion = GetComponent<CubeExplosion>();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        CreateCubes();       
+        Destroy(gameObject);
+    } 
+   
+    private void CreateCubes()
+    {
+        int chancePull = 100;
+        
+        if (_cloneChance > Random.Range(0, chancePull))
         {
             float cloneChanceDecrease = 0.5f;
             float scaleDecrease = 0.5f;
-            int cubesQuantity = Random.Range(2, 7);
+            int minCubesQuantity = 2;
+            int maxCubesQuantity = 6;
+            int cubesQuantity = Random.Range(minCubesQuantity, ++maxCubesQuantity);          
             List<Rigidbody> createdCubes = new();
+            Cube template = gameObject.GetComponent<Cube>();
 
             _cloneChance *= cloneChanceDecrease;
 
             for (int i = 0; i < cubesQuantity; i++)
             {
-                GameObject cube = Instantiate(gameObject, transform.position, Quaternion.identity);
+                Cube cube = Instantiate(template, transform.position, Quaternion.identity);
                 cube.transform.localScale = transform.localScale * scaleDecrease;
-                ChangeColor(cube.GetComponent<MeshRenderer>());
-                createdCubes.Add(cube.GetComponent<Rigidbody>());
-                yield return null;
+                ChangeColor(cube.MeshRenderer);               
+                createdCubes.Add(cube.Rigidbody);                
             }
 
-            foreach (Rigidbody createdCube in createdCubes)
-            {
-                createdCube.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
-                yield return null;
-            }
+            _explosion.BlowUpCubes(createdCubes);
         }
-
-        Destroy(gameObject);
     }
 
     private void ChangeColor(MeshRenderer cubeMeshRenderer)
---
+++ UnityProjects/3Dproject/Assets/Scripts/CubeExplosion.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CubeExplosion : MonoBehaviour
+{
+    [SerializeField] private float _explosionForce = 100;
+    [SerializeField] private float _explosionRadius = 100;
+
+    public void BlowUpCubes(List<Rigidbody> cubes)
+    {
+        foreach (Rigidbody cube in cubes)
+            cube.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+    }
+}
