fbda1a7a15cda16762363c4568f2f905a90744b0
bagabum640 <blackpig640@gmail.com>
2024-11-02 15:37:49 +0300

    new

--- UnityProjects/Platformer2D/Assets/Scripts/Environmets/Coin.cs
+++ UnityProjects/Platformer2D/Assets/Scripts/Environmets/Coin.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public event Action OnCreate;
+    public event Action<Vector2> OnCreate;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent<PlayerMovement>(out _))
         {
-            OnCreate?.Invoke();
+            OnCreate?.Invoke(transform.position);
             Destroy(gameObject);
         }
     }  
--- UnityProjects/Platformer2D/Assets/Scripts/Environmets/CoinSpawner.cs
+++ UnityProjects/Platformer2D/Assets/Scripts/Environmets/CoinSpawner.cs
@@ -5,21 +5,32 @@ public class CoinSpawner : MonoBehaviour
 {
     [SerializeField] private Coin _coin;
 
-    private void Start() =>
-        StartCoroutine(SpawnCoin());
+    private Vector2[] _spawners;
+    private int _spawnCount;
 
-    private void Create()
+    private void Start()
+    {
+        _spawners = new Vector2[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            _spawners[i] = transform.GetChild(i).position;
+            StartCoroutine(SpawnCoin(_spawners[i]));
+        }
+    }
+
+    private void Create(Vector2 position)
     {
         float spawnDelay = 1f;
 
-        StartCoroutine(SpawnCoin(spawnDelay));
+        StartCoroutine(SpawnCoin(position, spawnDelay));
     }
 
-    private IEnumerator SpawnCoin(float firstSpawnDelay = 0.1f)
+    private IEnumerator SpawnCoin(Vector2 position, float firstSpawnDelay = 0.1f)
     {
         yield return new WaitForSeconds(firstSpawnDelay);
 
-        Coin coin = Instantiate(_coin, transform.position, Quaternion.identity);
+        Coin coin = Instantiate(_coin, position, Quaternion.identity);
 
         coin.OnCreate += Create;
     }
